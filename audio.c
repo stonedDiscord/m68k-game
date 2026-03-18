0d19025d86252e8a1ec8f935e21d9377246ab89f
stonedDiscord <Tukz@gmx.de>
2026-03-18 22:47:52 +0100

    ym2149 io

--- audio.c
+++ audio.c
@@ -4,3 +4,13 @@ void write_ym2149(uint8_t reg, uint8_t value) {
     *audio_address = reg;
     *audio_data = value;
 }
+
+uint8_t read_ioa(void) {
+    *audio_address = YM_IO_A;
+    return *audio_address;
+}
+
+uint8_t read_iob(void) {
+    *audio_address = YM_IO_B;
+    return *audio_address;
+}
