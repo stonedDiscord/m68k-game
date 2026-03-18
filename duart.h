2dc3cabbaf01693435eac940371527978c2c14e0
stonedDiscord <Tukz@gmx.de>
2026-03-18 17:40:05 +0100

    indents

--- duart.h
+++ duart.h
@@ -5,39 +5,39 @@
 
 // MC68681 config
 #define DUART_BASE 0x800180 				/* Base of I/O port addresses */
-#define MR1A (* (volatile char *) (DUART_BASE+0) ) 	/* Mode register 1A 		(R/W) */
-#define MR2A (* (volatile char *) (DUART_BASE+0) ) 	/* Mode register 2A 		(R/W) */
-#define SRA  (* (volatile char *) (DUART_BASE+2) ) 	/* Status Register A 		(R)   */
-#define CSRA (* (char *) (DUART_BASE+2) ) 		/* Clock Select register A 	(W)   */
-#define CRA  (* (char *) (DUART_BASE+4) ) 		/* Command Register A 		(W)   */
-#define RBRA (* (volatile char *) (DUART_BASE+6) ) 	/* Receiver Buffer  A  	(R)   */
-#define TBRA (* (char *) (DUART_BASE+6) ) 		/* Transmiter Buffer A		(W)   */
-#define ACR  (* (volatile char *) (DUART_BASE+8) ) 	/* Auxiliary Control Register  (R/W) */
+#define MR1A  (* (volatile char *) (DUART_BASE+0) ) 	/* Mode register 1A 		 (R/W)  */
+#define MR2A  (* (volatile char *) (DUART_BASE+0) )     /* Mode register 2A 		 (R/W)  */
+#define SRA   (* (volatile char *) (DUART_BASE+2) )     /* Status Register A 		 (R)    */
+#define CSRA  (* (volatile char *) (DUART_BASE+2) )     /* Clock Select register A 	 (W)    */
+#define CRA   (* (volatile char *) (DUART_BASE+4) )     /* Command Register A 		 (W)    */
+#define RBRA  (* (volatile char *) (DUART_BASE+6) )     /* Receiver Buffer  A  	     (R)    */
+#define TBRA  (* (volatile char *) (DUART_BASE+6) )     /* Transmiter Buffer A		 (W)    */
+#define ACR   (* (volatile char *) (DUART_BASE+8) )     /* Auxiliary Control Register  (R/W)*/
 
-#define ISR   (* (volatile char *) (DUART_BASE+10) )  /* Interrupt Status Register (R)     */
-#define IMR   (* (char *) (DUART_BASE+10) ) 		/* Interrupt Mask Register   (W)     */
+#define ISR   (* (volatile char *) (DUART_BASE+10) )    /* Interrupt Status Register (R)    */
+#define IMR   (* (volatile char *) (DUART_BASE+10) ) 	/* Interrupt Mask Register   (W)    */
 
-#define CUR   (* (volatile char *) (DUART_BASE+12) ) 	/* Counter MSB               (R) */
-#define CTUR  (* (char *) (DUART_BASE+12) ) 	        /* Counter/Timer Upper Reg   (W) */
-#define CLR   (* (volatile char *) (DUART_BASE+14) ) 	/* Counter LSB               (R) */
-#define CTLR  (* (char *) (DUART_BASE+14) ) 	        /* Counter/Timer Lower Reg   (W) */
+#define CUR   (* (volatile char *) (DUART_BASE+12) ) 	/* Counter MSB               (R)    */
+#define CTUR  (* (volatile char *) (DUART_BASE+12) ) 	/* Counter/Timer Upper Reg   (W)    */
+#define CLR   (* (volatile char *) (DUART_BASE+14) ) 	/* Counter LSB               (R)    */
+#define CTLR  (* (volatile char *) (DUART_BASE+14) ) 	/* Counter/Timer Lower Reg   (W)    */
 
-#define MR1B  (* (volatile char *) (DUART_BASE+16) )  /* Mode Register 1B          (R/W)   */
-#define MR2B  (* (volatile char *) (DUART_BASE+16) )  /* Mode Register 2B          (R/W)   */
-#define SRB   (* (volatile char *) (DUART_BASE+18) )  /* Status Register B         (R)     */
-#define CSRB  (* (char *) (DUART_BASE+18) ) 		/* Clock Select Register B   (W)     */
-#define CRB   (* (char *) (DUART_BASE+20) ) 		/* Commands Register B       (W)     */
-#define RBRB   (* (volatile  char *) (DUART_BASE+22) ) /* Reciever Buffer B         (R)     */
-#define TBRB   (* (char *) (DUART_BASE+22) ) 		/* Transmitter Buffer B      (W)     */
+#define MR1B  (* (volatile char *) (DUART_BASE+16) )    /* Mode Register 1B          (R/W)  */
+#define MR2B  (* (volatile char *) (DUART_BASE+16) )    /* Mode Register 2B          (R/W)  */
+#define SRB   (* (volatile char *) (DUART_BASE+18) )    /* Status Register B         (R)    */
+#define CSRB  (* (volatile char *) (DUART_BASE+18) ) 	/* Clock Select Register B   (W)    */
+#define CRB   (* (volatile char *) (DUART_BASE+20) ) 	/* Commands Register B       (W)    */
+#define RBRB  (* (volatile char *) (DUART_BASE+22) )    /* Reciever Buffer B         (R)    */
+#define TBRB  (* (volatile char *) (DUART_BASE+22) ) 	/* Transmitter Buffer B      (W)    */
 
-#define IVR   (* (volatile char *) (DUART_BASE+24) )  /* Interrupt Vector Register (R/W)   */
+#define IVR   (* (volatile char *) (DUART_BASE+24) )    /* Interrupt Vector Register (R/W)  */
 
-#define IPUL   (* (volatile char *) (DUART_BASE+26) )  /* Unlatched Input Port values (R)  */
-#define OPCR   (* (char *) (DUART_BASE+26) )            /* Output Port Configuration Register (W)   */
+#define IPUL  (* (volatile char *) (DUART_BASE+26) )    /* Unlatched Input Port values (R)  */
+#define OPCR  (* (volatile char *) (DUART_BASE+26) )    /* Output Port Configuration Register (W)   */
 
-#define STRT_CNTR   (* (volatile char *) (DUART_BASE+28) )  /* Start-Counter Command (R)  */
+#define STRT_CNTR (* (volatile char *) (DUART_BASE+28) )  /* Start-Counter Command (R)  */
 #define OPR_SET   (* (volatile char *) (DUART_BASE+28) )  /* Output Port Bit Set Command (W)  */
-#define STOP_CNTR   (* (volatile char *) (DUART_BASE+30) )  /* Stop-Counter Command (R) / Clear timer interrupt  */
+#define STOP_CNTR (* (volatile char *) (DUART_BASE+30) )  /* Stop-Counter Command (R) / Clear timer interrupt  */
 #define OPR_CLR   (* (volatile char *) (DUART_BASE+30) )  /* Output Port Bit Clear Command (W)  */
 
 #define RxRDY 0x01	/* Receiver ready bit mask */
@@ -49,6 +49,6 @@
 // functions
 void setup_duart(void);
 char getchar_(void);
-void putchar_(char c);
+void putchar_(volatile char c);
 
 #endif /* DUART_H */
