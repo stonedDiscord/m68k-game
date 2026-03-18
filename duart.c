82755b20fdd8759571d124e1a8624e2274f15b03
stonedDiscord <Tukz@gmx.de>
2026-03-18 19:36:57 +0100

    init sequence

--- duart.c
+++ duart.c
@@ -11,33 +11,38 @@ duartInterrupt(void) {
 
 // duart MC68681
 void setup_duart(void) {
-	CRA = 0x30; /* Reset port A transmitter */
-	CRA = 0x20; /* Reset port A receiver */
-	CRA = 0x10; /* Reset port A mode register pointer */
-	//ACR = 0x00; /* Select Baud rate Set 1, no counter nor timer */
-	ACR = 0xF0; /* Select Baud rate Set 1, counter X1/16 */
-	CSRA = 0xBB; /* Set both the Rx and Tx speeds to 9600 baud */
-	MR1A = 0x93; /* Port A 8 bits, no parity, 1 stop bit enable RxRTS output */
-	//MR2A = 0x07; /* Basic operating mode */
-    MR2A = 0x37; /* Normal operating mode, enable TxRTS, TxCTS, 1 stop bit */
-    
-    // Set up the timer for 300Hz
-    // Crystal 3.6864MHz / 16 prescaler = 230400. 230400 / 300 = 768 ($300) (384? $180)
-    CTUR = 0x01; /* Set Timer range value */
-    CTLR = 0x80;
-    
-    // Place counter/timer output on OP3
-    OPCR = 0x04; 
+	OPCR = 0x00;
 
-    IMR = 0x0a; /* Enable Interrupts: RxRDYA interrupt (bit 1), Timer interrupt (bit 3) */
-    //IMR = 0x02; /* Enable Interrupts: RxRDYA interrupt (bit 1) */
-    IVR = 0x50; /* set vector number 0x50 (80d) that is at address 0x140 (320d) (see platform.ld) */
-	CRA = 0x05; /* Enable port A transmitter and receiver */
+	IMR  = 0x00;		// No interrupts enabled
+	ACR  = 0x00;
+
+	IVR  = 0x40;
+	CTUR = 0x07;
+	CTLR = 0x33;
+	CRA  = 0x1a;
+	CRA  = 0x2a;
+	CRA  = 0x3a;
+	CRB  = 0x1a;
+	CRB  = 0x2a;
+	CRB  = 0x3a;
+	OPR_SET = 0x60; // LEDs on
+
+	MR1A = 0x13;
+	MR2A = 0x07;
+	CSRA = 0xbb;
+	CRA  = 0x1a;
+
+	MR1B = 0x13;
+	MR2B = 0x07;
+	CSRB = 0xbb;
+	CRB  = 0x1a;
 }
 
 char getchar_(void) {
-	while ( (SRA & RxRDY) == 0 );
-	return RBRA;
+	if ( (SRA & RxRDY) == 0 )
+		return 0;
+	else
+		return RBRA;
 }
 
 void putchar_(char c) {
